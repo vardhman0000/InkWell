36e6b760be33ef4036d4a5cbaa9ef080e8ac3908
vardhman0000 <vardhman0000@gmail.com>
2024-05-17 18:15:04 +0530

    Added auth0 Authentication

--- notes-app/src/Components/Navbar.jsx
+++ notes-app/src/Components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import logo  from "../assets/logo.gif";
 import { Link } from 'react-router-dom'
 import LogoVideo from './LogoVideo';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   return (
     <div className='sticky top-0 z-50 bg-white'>
       <nav className='flex flex-row justify-around items-center shadow-lg'>
@@ -19,9 +21,25 @@ const Navbar = () => {
                 <Link className='font-semibold px-5 py-4 text-lg' to="/Contact">Contact</Link>
             </ul>
         </div>
-        <div className='w-48 flex justify-center'>
-            <button className='font-semibold text-lg px-6 py-3 rounded-full border-2 border-black hover:bg-black hover:text-white hover:scale-110'>Login</button>
+
+
+        <div className='flex flex-row items-center'>
+          <div className="userDetails gap-x-2 flex items-center">
+            {isAuthenticated && (<img className='rounded-full w-10' src={user.picture} alt ="pf"/>)}
+            {isAuthenticated && (<p>{user.nickname}</p>)}
+          </div>
+
+          <div className='w-48 flex flex-row justify-center items-center gap-x-10'>
+              {isAuthenticated ? (
+                <button className='font-semibold text-lg px-6 py-3 rounded-full border-2 border-black hover:bg-black hover:text-white hover:scale-110' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                  Log Out
+                </button>
+              ) : (
+                <button className='font-semibold text-lg px-6 py-3 rounded-full border-2 border-black hover:bg-black hover:text-white hover:scale-110' onClick={() => loginWithRedirect()}>Log In</button>
+              )}
+          </div>
         </div>
+          
       </nav>
     </div>
   )
--- notes-app/src/main.jsx
+++ notes-app/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />,
-  {/* </React.StrictMode>, */}
+  <Auth0Provider
+  domain="dev-6rem56bphs8i75qh.us.auth0.com"
+  clientId="t6szebgcYSnvGuDsU5ExJ3Yl5lrAfAxC"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  <App />
+</Auth0Provider>,
 )
